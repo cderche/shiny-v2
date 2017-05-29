3561d0a9d2a55e7c40196d424db41df65a4e075b
Kirill <c.derche@me.com>
2017-05-29 17:02:41 +0300

    product form completed.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
   enum category: [:room, :option, :material]
+
+  validates :title    , presence: true
+  validates :price    , presence: true
+  validates :category , presence: true
+  
 end
