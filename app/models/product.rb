454993ec6ed8b6f151408f04a36c5a36848ab42a
Kirill <c.derche@me.com>
2017-05-29 20:19:56 +0300

    line items model and protection

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,5 +4,18 @@ class Product < ApplicationRecord
   validates :title    , presence: true
   validates :price    , presence: true
   validates :category , presence: true
-  
+
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      throw :abort
+    end
+  end
+
 end
