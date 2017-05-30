4e45161ca80c8b05d93dcd3dc0171f010fa0d11b
Kirill <c.derche@me.com>
2017-05-30 18:38:41 +0300

    ui button fix for index.

--- app/helpers/icons_helper.rb
+++ app/helpers/icons_helper.rb
@@ -4,4 +4,12 @@ module IconsHelper
     content_tag(:i, nil, class: "fa #{name} #{options}")
   end
 
+  def icon_link(path, iKlass, options = {})
+    link_to(icon(iKlass), path, options)
+  end
+
+  def icon(klass)
+    content_tag(:i, nil, class: klass)
+  end
+
 end
