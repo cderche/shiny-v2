4c531a1a96337d45a271eb351531533ee0d99228
Kirill <c.derche@me.com>
2017-05-31 19:04:27 +0300

    fixed migration

--- db/migrate/20170531160356_add_token_to_carts.rb
+++ db/migrate/20170531160356_add_token_to_carts.rb
@@ -1,5 +1,6 @@
 class AddTokenToCarts < ActiveRecord::Migration[5.0]
   def change
     add_column :carts, :token, :string
+    add_index :carts, :token, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170531155948) do
+ActiveRecord::Schema.define(version: 20170531160356) do
 
   create_table "carts", force: :cascade do |t|
     t.text     "start_date"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170531155948) do
     t.integer  "user_id"
     t.string   "session"
     t.string   "token"
+    t.index ["token"], name: "index_carts_on_token", unique: true
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
